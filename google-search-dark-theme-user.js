9d8107c16e59e0981e157a487f0487f25e3ca500
hansaplasst <forum@logic-q.nl>
2026-08-31 02:09:24 +0200

    Add Google Search Dark Theme userscript

---
+++ google-search-dark-theme-user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Google Search Dark Theme
+// @namespace    http://tampermonkey.net/
+// @version      1.4
+// @description  Auto Enable Google Search Dark Theme
+// @author       Hansaplast
+// @include      https://www.google.*/*
+// @icon         https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico
+// @grant        none
+// @license      GPL3
+// @downloadURL https://update.greasyfork.org/scripts/570302/Google%20Search%20Dark%20Theme.user.js
+// @updateURL https://update.greasyfork.org/scripts/570302/Google%20Search%20Dark%20Theme.meta.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const observer = new MutationObserver(() => {
+        const darkThemeToggle = document.getElementsByClassName("tFYjZe")[0];
+        const themeIsLight = (window.getComputedStyle(document.body).backgroundColor === 'rgb(255, 255, 255)');
+
+        if (darkThemeToggle && themeIsLight) {
+            darkThemeToggle.click();
+        }
+
+        observer.disconnect(); // Stop the observer if the function is executed
+    });
+
+    const config = { childList: true, subtree: true };
+    observer.observe(document.body, config);
+})();
